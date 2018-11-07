6addcf906e51e86e31d4507d2fa4c0d37d554340
Helge Stentröm <h.stenstrom@gmail.com>
2018-11-07 23:04:38 +0100

    Chokladtest

--- src/main/java/tudelft/chocolate/ChocolateBags.java
+++ src/main/java/tudelft/chocolate/ChocolateBags.java
@@ -2,7 +2,7 @@ package tudelft.chocolate;
 
 class ChocolateBags {
 
-    int calculateNeededOnes(int ones, int fives, int total) {
+    int calculateNeededOnes1(int ones, int fives, int total) {
         int maxFives = total / 5;
         int fivesWeCanUse = fives <= maxFives ? fives : maxFives;
         total -= fivesWeCanUse * 5;
@@ -10,6 +10,16 @@ class ChocolateBags {
         final int failMarker = -1;
         return (ones < total) ? failMarker : total;
 
+    }
+
+
+    public int calculateNeededOnes(int small, int big, int goal) {
+         int maxBigBoxes = goal / 5;
+         int bigBoxesWeCanUse = maxBigBoxes < big ? maxBigBoxes : big;
+       goal -= (bigBoxesWeCanUse * 5);
+       if(small < goal)
+            return -1;
+        return goal;
     }
 
 }
--- src/test/java/tudelft/chocolate/ChocolateBagsHelge.java
+++ src/test/java/tudelft/chocolate/ChocolateBagsHelge.java
@@ -1,8 +1,6 @@
 package tudelft.chocolate;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -45,6 +43,27 @@ class ChocolateBagsHelge {
     void needFivesAndOnes(int ones, int fives, int total, int expected) {
         // Setup and execute
         int result = chocolateBags.calculateNeededOnes(ones, fives, total);
+
+        // Verify
+        assertEquals(expected, result);
+    }
+
+
+    @ParameterizedTest
+    @CsvSource({
+            // Three fives and two ones are needed for 17 in total. We have a surplus of ones or of fives
+            "2, 30, 17, 2",
+            "99, 3, 17, 2",
+            // Too few ones
+            "1, 30, 17, -1",
+            // To few fives
+            "2, 2, 17, -1",
+            // Exactly the right amount
+            "2, 3, 15, 0"
+    })
+    void mcFivesAndOnes(int ones, int fives, int total, int expected) {
+        // Setup and execute
+        int result = chocolateBags.calculateNeededOnes1(ones, fives, total);
 
         // Verify
         assertEquals(expected, result);
