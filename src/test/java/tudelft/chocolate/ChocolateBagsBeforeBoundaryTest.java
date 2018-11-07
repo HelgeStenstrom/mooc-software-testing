6a9d98516989d93af2d045865e29f44e39559f3f
Helge Stentröm <h.stenstrom@gmail.com>
2018-11-07 22:13:54 +0100

    Exercise

--- src/test/java/tudelft/chocolate/ChocolateBagsBeforeBoundaryTest.java
+++ src/test/java/tudelft/chocolate/ChocolateBagsBeforeBoundaryTest.java
@@ -1,31 +1,45 @@
 package tudelft.chocolate;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class ChocolateBagsBeforeBoundaryTest {
+
+    private ChocolateBags chocolateBags;
+
+    @BeforeEach
+    void setup() {
+        chocolateBags = new ChocolateBags();
+    }
+
+
     @Test
     void totalIsBiggerThanAmountOfBars() {
-        ChocolateBags bags = new ChocolateBags();
-        int result = bags.calculateNeededOnes(1, 1, 10);
+        int result = chocolateBags.calculateNeededOnes(1, 1, 10);
         Assertions.assertEquals(-1, result);
     }
 
     @Test
     void onlyBigBars() {
-        int result = new ChocolateBags().calculateNeededOnes(5, 3, 10);
+        int result = chocolateBags.calculateNeededOnes(500, 30, 10);
         Assertions.assertEquals(0, result);
     }
 
     @Test
     void bigAndSmallBars() {
-        int result = new ChocolateBags().calculateNeededOnes(5, 3, 17);
+        int result = chocolateBags.calculateNeededOnes(5, 3, 17);
         Assertions.assertEquals(2, result);
     }
 
     @Test
     void onlySmallBars() {
-        int result = new ChocolateBags().calculateNeededOnes(4, 2, 3);
+        int result = chocolateBags.calculateNeededOnes(4, 2, 3);
         Assertions.assertEquals(3, result);
+    }
+
+    @Test
+    void onlySmallBarsAgain() {
+        Assertions.assertEquals(3, chocolateBags.calculateNeededOnes(1000, 0, 3));
     }
 }
---
+++ src/test/java/tudelft/chocolate/ChocolateBagsHelge.java
@@ -0,0 +1,54 @@
+package tudelft.chocolate;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ChocolateBagsHelge {
+
+    private ChocolateBags chocolateBags;
+
+    @BeforeEach
+    void setup() {
+        chocolateBags = new ChocolateBags();
+    }
+
+
+    @ParameterizedTest(name = "ettor={0}, femmor={1}, total={2}, result={3}")
+    @CsvSource({
+            "1,1,5,0",
+            "1,1,6,1",
+            "1,1,7,-1",
+            "1, 1, 8, -1"
+    })
+    void totalIsTooBig(int ones, int fives, int total, int expected) {
+        int result = chocolateBags.calculateNeededOnes(ones, fives, total);
+        assertEquals(expected, result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            // Three fives and two ones are needed for 17 in total. We have a surplus of ones or of fives
+            "2, 30, 17, 2",
+            "99, 3, 17, 2",
+            // Too few ones
+            "1, 30, 17, -1",
+            // To few fives
+            "2, 2, 17, -1",
+            // Exactly the right amount
+            "2, 3, 15, 0"
+    })
+    void needFivesAndOnes(int ones, int fives, int total, int expected) {
+        // Setup and execute
+        int result = chocolateBags.calculateNeededOnes(ones, fives, total);
+
+        // Verify
+        assertEquals(expected, result);
+    }
+
+
+}
