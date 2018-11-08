ea0e7db6a7a4eb27c707c83d958ca361ab4d8ac5
Helge Stentröm <h.stenstrom@gmail.com>
2018-11-08 08:02:15 +0100

    Update src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java

--- src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
+++ src/test/java/tudelft/caesarshift/CaesarShiftCipherTest.java
@@ -1,5 +1,38 @@
 package tudelft.caesarshift;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CaesarShiftCipherTest {
 
+    CaesarShiftCipher cipher = new CaesarShiftCipher();
+
+    @Test
+    void noShift() {
+
+        String result = cipher.CaesarShiftCipher("abc", 0);
+
+        assertEquals("abc", result);
+    }
+
+    @Test
+    void shiftByOne() {
+
+        String result = cipher.CaesarShiftCipher("abc", 1);
+
+        assertEquals("bcd", result);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'', 0, '",
+    })
+    void someStrings(String string, int shift, String expected) {
+        
+
+    }
 }
