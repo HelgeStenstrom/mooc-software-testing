a4b2afb5a196abae91228ce111cb8e175ee35c9a
Helge Stentröm <h.stenstrom@gmail.com>
2018-11-23 22:51:26 +0100

    discount

--- src/test/java/tudelft/discount/DiscountApplierTest.java
+++ src/test/java/tudelft/discount/DiscountApplierTest.java
@@ -1,5 +1,47 @@
 package tudelft.discount;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class DiscountApplierTest {
 
+    Product business = new Product("bu", 100, "BUSINESS");
+    Product home = new Product("ho", 100, "HOME");
+
+
+    @Test
+    void bu() {
+        // Setup
+
+        ProductDao dao = Mockito.mock(ProductDao.class);
+        final DiscountApplier discountApplier = new DiscountApplier(dao);
+
+        Mockito.when(dao.all()).thenReturn(Arrays.asList(business, home));
+        // Exercise
+
+        discountApplier.setNewPrices();
+        // Verify
+
+        assertEquals(110, business.getPrice(), 0.01);
+    }
+
+    @Test
+    void setHome() {
+        // Setup
+
+        ProductDao dao = Mockito.mock(ProductDao.class);
+        final DiscountApplier discountApplier = new DiscountApplier(dao);
+
+        Mockito.when(dao.all()).thenReturn(Arrays.asList(business, home));
+        // Exercise
+
+        discountApplier.setNewPrices();
+
+        // Verify
+        assertEquals(90, home.getPrice(), 0.01);
+    }
 }
--- src/test/java/tudelft/sum/TwoNumbersSumTest.java
+++ src/test/java/tudelft/sum/TwoNumbersSumTest.java
@@ -36,6 +36,9 @@ class TwoNumbersSumTest {
             "1, 1,  1, 2,   2, 3",
             "1, 2,  1, 1,   2, 3",
             "1, 2,  1, 2,   2, 4",
+            "4, 4,  4, 6,   9, 0",
+            "4, 0,  1, 1,   5, 1",
+           // "8, 9,  1, 1,   0, 0",
     })
     void test2(int a1, int a2, int b1, int b2, int s1, int s2) {
         // Setup
@@ -51,4 +54,24 @@ class TwoNumbersSumTest {
         assertEquals(expexcted, adder.addTwoNumbers(a, b));
     }
 
+    // TODO: test with 3-digit results
+    // TODO: find the bug
+
+    @ParameterizedTest
+    @CsvSource({
+            "8, 9,  1, 1,   1, 0, 0",
+    })
+    void test3(int a1, int a2, int b1, int b2, int s1, int s2, int s3) {
+        // Setup
+        final ArrayList<Integer> a = new ArrayList<>(Arrays.asList(a1, a2));
+        final ArrayList<Integer> b = new ArrayList<>(Arrays.asList(b1, b2));
+        final ArrayList<Integer> expexcted = new ArrayList<>(Arrays.asList(s1, s2, s3));
+
+        final TwoNumbersSum adder = new TwoNumbersSum();
+
+        // Exercise
+
+        // Verify
+        assertEquals(expexcted, adder.addTwoNumbers(a, b));
+    }
 }
